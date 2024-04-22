c80964c433e066e4dd83ce44ff353ecb31c50d17
Nikita Zabelin <nikita.zabelin@kaspersky.com>
2024-04-22 20:13:57 +0300

    Some helpful controller commands

--- app/controller/src/controller.py
+++ app/controller/src/controller.py
@@ -5,6 +5,17 @@ import prometheus_api_client
 
 async def main():
     prom = prometheus_api_client.PrometheusConnect("http://localhost:8080", disable_ssl=True)
+    resp = prom.all_metrics()
+
+    increase = 'irate(aiohttp_request_duration_seconds_count{path_template = "/"}[1m])'
+    total_processor_time = 'process_cpu_seconds_total[1m]'
+
+    kubectl_get_pods_count = "kubectl get pods -l=app=app -o jsonpath={.items[*].metadata.generateName}"
+
+    
+
+
+    a = 12
 
 
 if __name__ == '__main__':
